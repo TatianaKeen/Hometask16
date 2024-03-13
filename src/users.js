c5516dc1bb238abe4364077b7e200416318ed9ba
tetiana <tprokopieva@logixits.com>
2024-03-13 14:13:09 -0400

    add styles

--- src/users.js
+++ src/users.js
@@ -2,21 +2,10 @@ function UsersService(baseUrl) {
   this.baseUrl = baseUrl;
 }
 
-document.addEventListener('DOMContentLoaded', async () => {
-  try {
-    const usersService = new UsersService('https://jsonplaceholder.typicode.com');
-    const users = await usersService.getAllUsers();
-    usersService.renderUsersList(users);
-  } catch (error) {
-    console.error('Error:', error);
-  }
-});
-
 UsersService.prototype.getAllUsers = async function () {
   try {
     const response = await fetch(`${this.baseUrl}/users`);
-    const users = await response.json();
-    return users;
+    return response.json();
   } catch (error) {
     console.error('Error fetching users:', error);
     throw new Error('Error fetching users');
@@ -25,9 +14,13 @@ UsersService.prototype.getAllUsers = async function () {
 
 UsersService.prototype.getUserById = async function (id) {
   try {
+    await new Promise(function (resolve, reject) {
+      setTimeout(function () {
+        resolve()
+      }, 500);
+    });
     const response = await fetch(`${this.baseUrl}/users/${id}`);
-    const user = await response.json();
-    return user;
+    return response.json();
   } catch (error) {
     console.error(`Error fetching user with id ${id}:`, error);
     throw new Error(`Error fetching user with id ${id}`);
@@ -36,6 +29,7 @@ UsersService.prototype.getUserById = async function (id) {
 
 UsersService.prototype.renderUsersList = function (list) {
   const userListElement = document.getElementById('user-list');
+  const loadingElement = document.getElementById('loading');
   userListElement.innerHTML = '';
 
   list.forEach(user => {
@@ -46,17 +40,16 @@ UsersService.prototype.renderUsersList = function (list) {
     userAvatar.classList.add('user-avatar');
     listItem.appendChild(userAvatar);
     listItem.innerHTML += `
-      <h2>${user.name}</h2>
-      <p>Company: ${user.company.name}</p>
+      <div>
+        <h3>${user.name}</h2>
+        <p>Company: ${user.company.name}</p>
+      </div>
     `;
+    listItem.classList.add('main-info');
     listItem.addEventListener('click', async () => {
-      try {
-        const userInfo = await this.getUserById(user.id);
-        this.renderUserDetails(userInfo);
-      } catch (error) {
-        console.error('Error fetching user information:', error);
-        alert('Error fetching user information');
-      }
+      this.renderLoading();
+      const userInfo = await this.getUserById(user.id);
+      this.renderUserDetails(userInfo);
     });
     userListElement.appendChild(listItem);
   });
@@ -65,15 +58,38 @@ UsersService.prototype.renderUsersList = function (list) {
 UsersService.prototype.renderUserDetails = function (user) {
   const userDetailsElement = document.getElementById('user-details');
   userDetailsElement.innerHTML = `
+  <div class="main-info">
     <img class="user-avatar" src="https://api.lorem.space/image/face?w=50&h=50&r=${user.id}" alt="Avatar of ${user.name}">
+    <div>
+      <h2>${user.name}</h2>
+      <p>Username: ${user.username}</p>
+    </div>
+  </div>
 
-    <h2>${user.name}</h2>
-    <p>Username: ${user.username}</p>
-
-    <p>Company: ${user.company.name}</p>
-    <p>Phone: ${user.phone}</p>
-    <p>Email: ${user.email}</p>
-    <p>Address: ${user.address.suite}, ${user.address.street}, ${user.address.city} / ${user.address.zipcode}</p>
-    <hr>
+  <div class="detail-info">
+    <div class="row">
+      <p>Company</p>
+      <p>${user.company.name}</p>
+    </div>
+    <div class="row">
+      <p>Phone</p>
+      <p>${user.phone}</p>
+    </div>
+    <div class="row">
+      <p>Email</p>
+      <a href="mailto:${user.email}">${user.email}</a>
+    </div>
+    <div class="row">
+        <p>Address</p>
+        <p>${user.address.suite}, ${user.address.street}, ${user.address.city} / ${user.address.zipcode}</p>
+    </div>
+  </div>
   `;
 };
+
+
+UsersService.prototype.renderLoading = function () {
+  const userDetailsElement = document.getElementById('user-details');
+  userDetailsElement.innerHTML = `<p class="loading">Loading...</p>`;
+};
+
